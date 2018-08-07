ce64c0471f971484ed68a129356162fc97fb2182
Arnaud VINAS <avinas@e3r11p15.42.fr>
2018-08-07 14:55:21 +0200

    debug fichier texture absent fait

--- src/utils/usage.c
+++ src/utils/usage.c
@@ -26,6 +26,13 @@ void		ft_malloc_error(t_env *e)
 	exit(1);
 }
 
+void		ft_texture_error(t_env *e)
+{
+	ft_putstr_fd("Problem with texture file\n", 2);
+	free(e);
+	exit(1);
+}
+
 void		exit_message(char *str)
 {
 	ft_putstr_fd(str, 2);
